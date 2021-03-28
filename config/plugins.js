e6854ae71c73416abbc3ff8e912e2d7001fad1d0
Eran Sakal <eran@sakalim.com>
2021-03-29 01:58:55 +0300

    amazon images

--- config/plugins.js
+++ config/plugins.js
@@ -1,28 +1,25 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'aws-s3-resizing-and-optimisation',
+    provider: 'aws-s3-enhanced',
     providerOptions: {
       accessKeyId: env('AWS_ACCESS_KEY_ID'),
       secretAccessKey: env('AWS_ACCESS_SECRET'),
       region: env('AWS_REGION'),
-      optimizeOptions: {
-        jpeg: {
-          quality: 90,
-          progressive: true,
-        },
-        png: {
-          quality: 90,
-          progressive: true,
-        },
-        webp: {
-          alphaQuality: 90,
-        },
-        tiff: {}
-      },
-      imageSizes: [],
       params: {
         Bucket: env('AWS_BUCKET_NAME'),
       },
+      prefix: null,
+      quality: 80,
+      webp: false,
+      thumbnails: [
+        {
+          name: 'custom',
+          options: {
+            width: 1200,
+            withoutEnlargement: true,
+          },
+        }
+      ],
     },
-  }
+  },
 });
