6e9c39ae1efb12d5c051e5880e399124a25e1075
Eran Sakal <eran@sakalim.com>
2021-04-01 18:19:16 +0300

    update package.json

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => {
     port: env.int('PORT', 1337),
     admin: {
       auth: {
-        secret: env('ADMIN_JWT_SECRET', '1656359e54a3kdn2fcfa3d90dcg8dce9e'),
+          secret: env('ADMIN_JWT_SECRET', '1656359e54a3kdn2fcfa3d90dcg8dce9e'),
       },
     },
   })
